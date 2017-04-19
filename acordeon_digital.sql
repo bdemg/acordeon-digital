cf7ec215a678127a84f784b518cda1e59113d4f5
Jorge Cano <jacano.sosa@gmail.com>
2017-04-19 16:57:24 -0500

    añadida estructura de base de datos

---
+++ acordeon_digital.sql
@@ -0,0 +1,138 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.5.2
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 19-04-2017 a las 23:50:40
+-- Versión del servidor: 10.1.21-MariaDB
+-- Versión de PHP: 5.6.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `acordeon_digital`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `concepts`
+--
+
+CREATE TABLE `concepts` (
+  `concept_id` int(11) NOT NULL,
+  `concept_name` varchar(25) NOT NULL,
+  `category` varchar(25) NOT NULL,
+  `definition` varchar(500) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `creation`
+--
+
+CREATE TABLE `creation` (
+  `user_id` int(11) NOT NULL,
+  `concept_id` int(11) NOT NULL,
+  `creation_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `editions`
+--
+
+CREATE TABLE `editions` (
+  `user_id` int(11) NOT NULL,
+  `concept_id` int(11) NOT NULL,
+  `edit_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `users`
+--
+
+CREATE TABLE `users` (
+  `user_id` int(11) NOT NULL,
+  `name` varchar(30) NOT NULL,
+  `password` varchar(27) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `concepts`
+--
+ALTER TABLE `concepts`
+  ADD PRIMARY KEY (`concept_id`);
+
+--
+-- Indices de la tabla `creation`
+--
+ALTER TABLE `creation`
+  ADD PRIMARY KEY (`user_id`,`concept_id`,`creation_date`),
+  ADD KEY `concept_id` (`concept_id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indices de la tabla `editions`
+--
+ALTER TABLE `editions`
+  ADD PRIMARY KEY (`user_id`,`concept_id`,`edit_date`),
+  ADD KEY `concept_id` (`concept_id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indices de la tabla `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `concepts`
+--
+ALTER TABLE `concepts`
+  MODIFY `concept_id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `users`
+--
+ALTER TABLE `users`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `concepts`
+--
+ALTER TABLE `concepts`
+  ADD CONSTRAINT `concepts2creation` FOREIGN KEY (`concept_id`) REFERENCES `creation` (`concept_id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `concepts2edition` FOREIGN KEY (`concept_id`) REFERENCES `editions` (`concept_id`) ON DELETE CASCADE;
+
+--
+-- Filtros para la tabla `users`
+--
+ALTER TABLE `users`
+  ADD CONSTRAINT `users2creations` FOREIGN KEY (`user_id`) REFERENCES `creation` (`user_id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `users2editions` FOREIGN KEY (`user_id`) REFERENCES `editions` (`user_id`) ON DELETE CASCADE;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
