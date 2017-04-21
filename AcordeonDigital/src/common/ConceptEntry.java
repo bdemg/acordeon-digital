aa93dc6daa5e80cad8827633dc5c1044191a4e7a
Jorge Cano <jacano.sosa@gmail.com>
2017-04-21 10:05:08 -0500

    Interfaz tentativa para el servidor y un bean serializable para representar los conceptos

---
+++ AcordeonDigital/src/common/ConceptEntry.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package common;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author Jorge A. Cano
+ */
+public class ConceptEntry implements Serializable{
+    
+    private String conceptName;
+    private String category;
+    private String definition;
+
+    public ConceptEntry(String conceptName, String category, String definition) {
+        this.conceptName = conceptName;
+        this.category = category;
+        this.definition = definition;
+    }
+
+    /**
+     * @return the conceptName
+     */
+    public String getConceptName() {
+        return conceptName;
+    }
+
+    /**
+     * @param conceptName the conceptName to set
+     */
+    public void setConceptName(String conceptName) {
+        this.conceptName = conceptName;
+    }
+
+    /**
+     * @return the category
+     */
+    public String getCategory() {
+        return category;
+    }
+
+    /**
+     * @param category the category to set
+     */
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    /**
+     * @return the definition
+     */
+    public String getDefinition() {
+        return definition;
+    }
+
+    /**
+     * @param definition the definition to set
+     */
+    public void setDefinition(String definition) {
+        this.definition = definition;
+    }
+    
+    
+}
---
+++ AcordeonDigital/src/server/SeverInterface.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package server;
+
+import common.ConceptEntry;
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+/**
+ *
+ * @author Jorge A. Cano
+ */
+public interface SeverInterface extends Remote {
+    
+    public int getUserId(String userName, String password) throws RemoteException;
+    
+    public void addNewConcept(ConceptEntry conceptEntry, int userId) throws RemoteException;
+    
+    public ConceptEntry getConceptEntry(String conceptName) throws RemoteException;
+    
+    public ConceptEntry[] getAllConceptEntrys() throws RemoteException;
+    
+    public void updateConceptDefinition(ConceptEntry updatedConceptEntry, int userId) throws RemoteException;
+    
+    public void deleteConcepEntry(ConceptEntry conceptEntry, int userId) throws RemoteException;
+}
