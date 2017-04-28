5cf33ecfac5ca8f0aff91cbfddb3bdf8d27654fb
Jorge Cano <jacano.sosa@gmail.com>
2017-04-28 09:22:23 -0500

    Creación del DAO para consultar la tabla de usuarios

---
+++ AcordeonDigital/src/server/daos/UsersDAO.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package server.daos;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Jorge A. Cano
+ */
+public class UsersDAO extends DatabaseDAO{
+    
+    private static UsersDAO usersDAO;
+    
+    private final String GET_USER_ID = 
+            "SELECT user_id FROM users WHERE name=? AND password=?";
+    
+    private final int USER_NOT_FOUND = -420;
+    
+    private UsersDAO() throws SQLException{
+        
+        super();
+    }
+    
+    public static UsersDAO getCreationsDAO() throws SQLException{
+        
+        if(usersDAO == null){
+            usersDAO = new UsersDAO();
+        }
+        
+        return usersDAO;
+    }
+    
+    public int getUserID(String userName, String password) throws SQLException{
+        
+        PreparedStatement queryStatement = (PreparedStatement)
+                super.connectionToDatabase.prepareStatement( this.GET_USER_ID );
+        
+        queryStatement.setString(1, userName);
+        queryStatement.setString(2, password);
+        
+        ResultSet resultSet = queryStatement.executeQuery();
+        
+        boolean wasUsernameAndPasswordCorrect = resultSet.last();
+        
+        if(wasUsernameAndPasswordCorrect){
+            
+            return resultSet.getInt("user_id");
+        }
+        else{
+            
+            return this.USER_NOT_FOUND;
+        }
+    }
+}
